81e28f0a037e4d7b4630ce3557d4d6911a373990
jessicahattig <jessicahattig@gmail.com>
2023-07-21 17:05:04 -0700

    Function 1, test 2.

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -21,18 +21,18 @@ describe ('Game', () => {
 
     
 
-    // test ('It should take a valid number as input into the board array', () => {
-    //     game.placeNumber(1, game.board[0[0]]);
-    //     game.placeNumber(2, game.board[0[1]]);
-    //     game.placeNumber(3, game.board[8[8]]);
-    //     expect(game.board).toEqual([[1, 2, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    //                                 [0, 0, 0, 0, 0, 0, 0, 0, 3]])
-    // });
+    test ('It should take a valid number as input into the board array', () => {
+        game.placeNumber(1, game.board[0[0]]);
+        game.placeNumber(2, game.board[0[1]]);
+        game.placeNumber(3, game.board[8[8]]);
+        expect(game.board).toEqual([[1, 2, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 3]])
+    });
 })
